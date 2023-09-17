15b7851e94f8fd0398f5356b70739c242cb5a71c
Sergio Vidales Perez <srvidales@gmail.com>
2023-09-17 02:43:03 -0700

    Put back role table foreign key

--- db/schemas.sql
+++ db/schemas.sql
@@ -18,13 +18,10 @@ CREATE TABLE role
     id            INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title         VARCHAR(30) NOT NULL,
     salary        DECIMAL     NOT NULL,
-    department_id INT         NOT NULL
+    department_id INT         NOT NULL,
+    FOREIGN KEY (department_id)
+        REFERENCES department (id)
 );
-
-    -- FOREIGN KEY (department_id)
-    --     REFERENCES department (id)
-    --     ON DELETE CASCADE
-
 
 CREATE TABLE employee
 (
